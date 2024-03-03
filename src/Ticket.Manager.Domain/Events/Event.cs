a3a1ec9dd9d6d2436a72cc36b46acd688c3ceb52
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-04 00:13:26 +0100

    Add Event Entity

---
+++ src/Ticket.Manager.Domain/Events/Event.cs
@@ -0,0 +1,23 @@
+﻿using Ticket.Manager.Domain.Common.Domain;
+
+namespace Ticket.Manager.Domain.Events;
+
+public class Event : Entity, IAggregateRoot
+{
+    public Guid Id { get; private set; }
+    
+    public string Name { get; private set; }
+    
+    public DateTime StartDate { get; private set; }
+    
+    public DateTime StartOfSalesDate { get; private set; }
+    
+    public bool IsCanceled { get; private set; }
+    
+    public bool IsSuspended { get; private set; }
+    
+    private Event()
+    {
+        // EF
+    }
+}
