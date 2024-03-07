50e61682f53b8987d9c18dbcb9764bf41dc459bb
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-07 23:43:58 +0100

    Add CancelEventCommand

---
+++ src/Ticket.Manager.Application/Events/Cancel/CancelEventCommand.cs
@@ -0,0 +1,6 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Domain.Common;
+
+namespace Ticket.Manager.Application.Events.Cancel;
+
+public record CancelEventCommand(Guid EventId) : ICommand<Result>;
---
+++ src/Ticket.Manager.Application/Events/Cancel/CancelEventCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Application.Events.Errors;
+using Ticket.Manager.Domain.Common;
+using Ticket.Manager.Domain.Events;
+
+namespace Ticket.Manager.Application.Events.Cancel;
+
+public class CancelEventCommandHandler(IEventRepository eventRepository) : ICommandHandler<CancelEventCommand, Result>
+{
+    public async Task<Result> Handle(CancelEventCommand command, CancellationToken cancellationToken)
+    {
+        var @event = await eventRepository.Get(command.EventId, cancellationToken);
+        if (@event is null)
+        {
+            return Result.Failure(EventApplicationErrors.EventDoesntExist);
+        }
+
+        var result = @event.Cancel();
+
+        return result.IsFailure 
+            ? Result.Failure(result.Error) 
+            : Result.Success();
+    }
+}
