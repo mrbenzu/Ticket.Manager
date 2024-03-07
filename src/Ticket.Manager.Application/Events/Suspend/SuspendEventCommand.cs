85d8a1986f91dc168788bccdc5104f8dab4fbbc4
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-07 23:41:46 +0100

    Add SuspendEventCommand

---
+++ src/Ticket.Manager.Application/Events/Suspend/SuspendEventCommand.cs
@@ -0,0 +1,6 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Domain.Common;
+
+namespace Ticket.Manager.Application.Events.Suspend;
+
+public record SuspendEventCommand(Guid EventId) : ICommand<Result>;
---
+++ src/Ticket.Manager.Application/Events/Suspend/SuspendEventCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Application.Events.Errors;
+using Ticket.Manager.Domain.Common;
+using Ticket.Manager.Domain.Events;
+
+namespace Ticket.Manager.Application.Events.Suspend;
+
+public class SuspendEventCommandHandler(IEventRepository eventRepository) : ICommandHandler<SuspendEventCommand, Result>
+{
+    public async Task<Result> Handle(SuspendEventCommand command, CancellationToken cancellationToken)
+    {
+        var @event = await eventRepository.Get(command.EventId, cancellationToken);
+        if (@event is null)
+        {
+            return Result.Failure(EventApplicationErrors.EventDoesntExist);
+        }
+
+        var result = @event.Suspend();
+
+        return result.IsFailure 
+            ? Result.Failure(result.Error) 
+            : Result.Success();
+    }
+}
