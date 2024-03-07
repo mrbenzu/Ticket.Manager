568c7a08a8584107e139a5421f7871073b99d9f0
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-08 00:04:27 +0100

    Add ReopenEventCommand

---
+++ src/Ticket.Manager.Application/Events/Reopen/ReopenEventCommand.cs
@@ -0,0 +1,6 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Domain.Common;
+
+namespace Ticket.Manager.Application.Events.Reopen;
+
+public record ReopenEventCommand(Guid EventId) : ICommand<Result>;
---
+++ src/Ticket.Manager.Application/Events/Reopen/ReopenEventCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Application.Events.Errors;
+using Ticket.Manager.Domain.Common;
+using Ticket.Manager.Domain.Events;
+
+namespace Ticket.Manager.Application.Events.Reopen;
+
+public class ReopenEventCommandHandler(IEventRepository eventRepository) : ICommandHandler<ReopenEventCommand, Result>
+{
+    public async Task<Result> Handle(ReopenEventCommand command, CancellationToken cancellationToken)
+    {
+        var @event = await eventRepository.Get(command.EventId, cancellationToken);
+        if (@event is null)
+        {
+            return Result.Failure(EventApplicationErrors.EventDoesntExist);
+        }
+
+        var result = @event.Reopen();
+
+        return result.IsFailure 
+            ? Result.Failure(result.Error) 
+            : Result.Success();
+    }
+}
