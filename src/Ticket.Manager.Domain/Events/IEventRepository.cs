29d2a633fce614a94b26e9ef4a087e9d316d12e7
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-07 19:59:02 +0100

    Add IEventRepository

---
+++ src/Ticket.Manager.Domain/Events/IEventRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Ticket.Manager.Domain.Events;
+
+public interface IEventRepository
+{
+    Task Add(Event @event, CancellationToken cancellationToken);
+    Task<Event?> Get(Guid id, CancellationToken cancellationToken);
+}
