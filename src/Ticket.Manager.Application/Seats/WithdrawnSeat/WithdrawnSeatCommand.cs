9716c2ff9b414e17a1a84e681e4546a3bc244490
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-11 00:03:39 +0100

    Add WithdrawnSeatCommand

---
+++ src/Ticket.Manager.Application/Seats/WithdrawnSeat/WithdrawnSeatCommand.cs
@@ -0,0 +1,6 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Domain.Common;
+
+namespace Ticket.Manager.Application.Seats.WithdrawnSeat;
+
+public record WithdrawnSeatCommand(Guid SeatId) : ICommand<Result>;
---
+++ src/Ticket.Manager.Application/Seats/WithdrawnSeat/WithdrawnSeatCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Application.Seats.Errors;
+using Ticket.Manager.Domain.Common;
+using Ticket.Manager.Domain.Places;
+
+namespace Ticket.Manager.Application.Seats.WithdrawnSeat;
+
+public class WithdrawnSeatCommandHandler(ISeatRepository seatRepository) : ICommandHandler<WithdrawnSeatCommand, Result>
+{
+    public async Task<Result> Handle(WithdrawnSeatCommand command, CancellationToken cancellationToken)
+    {
+        var seat = await seatRepository.Get(command.SeatId, cancellationToken);
+        if (seat is null)
+        {
+            return Result.Failure(SeatApplicationErrors.SeatDoesntExist);
+        }
+
+        seat.Withdrawn();
+
+        return Result.Success();
+    }
+}
