ffef27a71315ef596cf9c3c79481505abdfd28f2
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-10 21:03:49 +0100

    Create SeatRepository

---
+++ src/Ticket.Manager.Domain/Places/ISeatRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ticket.Manager.Domain.Seats;
+
+namespace Ticket.Manager.Domain.Places;
+
+public interface ISeatRepository
+{
+    Task<Seat?> Get(Guid id, CancellationToken cancellationToken);
+}
