8c39fc04d57715bc0963427035953eede5b19a4c
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-10 23:42:07 +0100

    Add CancelReservationCommand

---
+++ src/Ticket.Manager.Application/Seats/CancelReservation/CancelReservationCommand.cs
@@ -0,0 +1,6 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Domain.Common;
+
+namespace Ticket.Manager.Application.Seats.CancelReservation;
+
+public record CancelReservationCommand(Guid SeatId) : ICommand<Result>;
---
+++ src/Ticket.Manager.Application/Seats/CancelReservation/CancelReservationCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Application.Seats.Errors;
+using Ticket.Manager.Domain.Common;
+using Ticket.Manager.Domain.Places;
+
+namespace Ticket.Manager.Application.Seats.CancelReservation;
+
+public class CancelReservationCommandHandler(ISeatRepository seatRepository) : ICommandHandler<CancelReservationCommand, Result>
+{
+    public async Task<Result> Handle(CancelReservationCommand command, CancellationToken cancellationToken)
+    {
+        var seat = await seatRepository.Get(command.SeatId, cancellationToken);
+        if (seat is null)
+        {
+            return Result.Failure(SeatApplicationErrors.SeatDoesntExist);
+        }
+
+        seat.CancelReservation();
+
+        return Result.Success();
+    }
+}
