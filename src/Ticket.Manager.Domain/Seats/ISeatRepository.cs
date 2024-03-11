731dcc47fddfb429dafc40c5da1677f90dfddeb1
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-11 22:37:26 +0100

    Update ISeatRepository

--- src/Ticket.Manager.Domain/Seats/ISeatRepository.cs
+++ src/Ticket.Manager.Domain/Seats/ISeatRepository.cs
@@ -1,9 +1,9 @@
-﻿using Ticket.Manager.Domain.Seats;
-
-namespace Ticket.Manager.Domain.Places;
+﻿namespace Ticket.Manager.Domain.Seats;
 
 public interface ISeatRepository
 {
+    Task AddMany(IEnumerable<Seat> seats, CancellationToken cancellationToken);
+    
     Task<Seat?> Get(Guid id, CancellationToken cancellationToken);
     
     Task<IEnumerable<int>> GetReservedSeatNumbersInRow(Guid eventId, SeatDetails seatDetails, CancellationToken cancellationToken);
