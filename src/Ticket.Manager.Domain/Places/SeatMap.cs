901a236940ec98ece107c69b4e53be58200a2626
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-11 22:43:52 +0100

    Update SeatMap for Place

--- src/Ticket.Manager.Domain/Places/SeatMap.cs
+++ src/Ticket.Manager.Domain/Places/SeatMap.cs
@@ -6,18 +6,20 @@ public class SeatMap : Entity
 {
     public long Id { get; private set; }
     public int NoNumericPlaceCount { get; private set; }
+    public int SectorCount { get; private set; }
     public int RowsCount { get; private set; }
     public int SeatsInRowCount { get; private set; }
 
-    private SeatMap(int noNumericPlaceCount, int rowsCount, int seatsInRowCount)
+    private SeatMap(int noNumericPlaceCount, int sectorCount, int rowsCount, int seatsInRowCount)
     {
         NoNumericPlaceCount = noNumericPlaceCount;
+        SectorCount = sectorCount;
         RowsCount = rowsCount;
         SeatsInRowCount = seatsInRowCount;  
     }
 
     public static SeatMap Create(int noNumericPlaceCount, int sectorCount, int rowsCount, int seatsInRowCount)
     {
-        return new SeatMap(noNumericPlaceCount, rowsCount, seatsInRowCount);
+        return new SeatMap(noNumericPlaceCount, sectorCount, rowsCount, seatsInRowCount);
     }
 }
