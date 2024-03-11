edfbcc42368201ce624099a5b662329072c7bc51
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-11 22:17:18 +0100

    CreateEventCommand change placeId type to Guid

--- src/Ticket.Manager.Application/Events/Create/CreateEventCommand.cs
+++ src/Ticket.Manager.Application/Events/Create/CreateEventCommand.cs
@@ -3,4 +3,4 @@ using Ticket.Manager.Domain.Common;
 
 namespace Ticket.Manager.Application.Events.Create;
 
-public record CreateEventCommand(string Name, DateTime StartDate, DateTime StartOfSalesDate, long PlaceId) : ICommand<Result>;
+public record CreateEventCommand(string Name, DateTime StartDate, DateTime StartOfSalesDate, Guid PlaceId) : ICommand<Result>;
