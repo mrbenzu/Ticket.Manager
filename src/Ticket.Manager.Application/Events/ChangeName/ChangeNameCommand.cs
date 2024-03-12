db6f9703a8bbe91286baf6b96ac800bcacd34e1a
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-12 23:02:31 +0100

    Add ChangeNameCommand

---
+++ src/Ticket.Manager.Application/Events/ChangeName/ChangeNameCommand.cs
@@ -0,0 +1,6 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Domain.Common;
+
+namespace Ticket.Manager.Application.Events.ChangeName;
+
+public record ChangeNameCommand(Guid EventId, string Name) : ICommand<Result>;
---
+++ src/Ticket.Manager.Application/Events/ChangeName/ChangeNameCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Ticket.Manager.Application.Common;
+using Ticket.Manager.Application.Events.Errors;
+using Ticket.Manager.Domain.Common;
+using Ticket.Manager.Domain.Events;
+
+namespace Ticket.Manager.Application.Events.ChangeName;
+
+public class ChangeNameCommandHandler(IEventRepository eventRepository) : ICommandHandler<ChangeNameCommand, Result>
+{
+    public async Task<Result> Handle(ChangeNameCommand command, CancellationToken cancellationToken)
+    {
+        var @event = await eventRepository.Get(command.EventId, cancellationToken);
+        if (@event is null)
+        {
+            return Result.Failure(EventApplicationErrors.EventDoesntExist);
+        }
+
+        var result = @event.ChangeName(command.Name);
+
+        return result;
+    }
+}
