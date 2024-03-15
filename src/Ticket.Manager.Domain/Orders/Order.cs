dcb07a908e4e5d27473821717686d5a94af4f133
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-15 18:53:56 +0100

    Add Approve order method

--- src/Ticket.Manager.Domain/Orders/Order.cs
+++ src/Ticket.Manager.Domain/Orders/Order.cs
@@ -50,4 +50,18 @@ public class Order : Entity, IAggregateRoot
 
         return Result.Success();
     }
+
+    public Result Approve(Guid userId)
+    {
+        if (UserId != userId)
+        {
+            return Result.Failure(OrderErrors.IsNotUserOrder);
+        }
+
+        IsPaid = true;
+        
+        AddDomainEvent(new OrderApprovedEvent(Id, _seats.Select(x => x.SeatId)));
+
+        return Result.Success();
+    }
 }
