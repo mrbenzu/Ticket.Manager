a4b093b655a7e6dafb7f7906408c35e56aac9802
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-16 17:48:12 +0100

    Add TicketManagerDbContext

---
+++ src/Ticket.Manager.Infrastructure/Configuration/TicketManagerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Ticket.Manager.Domain.Events;
+using Ticket.Manager.Domain.Orders;
+using Ticket.Manager.Domain.Places;
+
+namespace Ticket.Manager.Infrastructure.Configuration;
+
+public class TicketManagerDbContext(DbContextOptions<TicketManagerDbContext> options) : DbContext(options)
+{
+    public DbSet<Event> Events => Set<Event>();
+    public DbSet<Place> Places => Set<Place>();
+    public DbSet<Seat> Seats => Set<Seat>();
+    public DbSet<Order> Orders => Set<Order>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
