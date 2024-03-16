d508b3f3ba271c93de5752b7f3c73b6ac667d10c
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-03-16 18:12:01 +0100

    Add OrderModule

---
+++ src/Ticket.Manager.API/Orders/OrderModule.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Ticket.Manager.Application.Orders.Commands.CreateOrder;
+using Ticket.Manager.Application.Orders.Commands.ReturnOrder;
+using Ticket.Manager.Domain.Common;
+
+namespace Ticket.Manager.API.Orders;
+
+public static class OrderModule
+{
+    public static void AddPlaceEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("/create", async (ISender sender, CreateOrderCommand command) =>
+            {
+                var result = await sender.Send(command);
+
+                return Result(result);
+            })
+            .WithName("Create")
+            .WithOpenApi();
+        
+        app.MapPost("/return", async (ISender sender, ReturnOrderCommand command) =>
+            {
+                var result = await sender.Send(command);
+
+                return Result(result);
+            })
+            .WithName("Return")
+            .WithOpenApi();
+    }
+
+    private static IResult Result(Result result) =>
+        result.IsSuccess ? Results.Ok() : Results.BadRequest(result.Error.Description);
+}
