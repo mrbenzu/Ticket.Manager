1b6df2af96c934c6e82f7b3246557dd0c2d92757
Mateusz Modelski <42185582+mrbenzu@users.noreply.github.com>
2024-08-18 15:07:58 +0200

    Add Event mapping EF

---
+++ src/Ticket.Manager.Infrastructure/Events/EventConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ticket.Manager.Domain.Events;
+
+namespace Ticket.Manager.Infrastructure.Events;
+
+public class EventConfiguration : IEntityTypeConfiguration<Event>
+{
+    public void Configure(EntityTypeBuilder<Event> builder)
+    {
+        builder.ToTable("Events");
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(e => e.PlaceId)
+            .IsRequired();
+
+        builder.ComplexProperty(e => e.UnnumberedSeatsMap);
+        
+        builder.ComplexProperty(e => e.SeatsMap);
+
+        builder.Property(e => e.StartDate)
+            .IsRequired();
+
+        builder.Property(e => e.StartOfSalesDate)
+            .IsRequired();
+
+        builder.Property(e => e.IsCanceled)
+            .IsRequired();
+
+        builder.Property(e => e.IsSuspended)
+            .IsRequired();
+    }
+}
